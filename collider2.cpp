83817e482126e931844d2a464638a4d7b5f76400
yelzhas <yelzhx@mail.ru>
2017-11-08 02:20:57 +0600

    Update collider2.cpp

--- collider2.cpp
+++ collider2.cpp
@@ -1,3 +1,6 @@
+#include "myobj.h"
+....
+
 EXPORT bool  check_pos(float x,float y,float z,float h){ // x,y,z координаты,  h- высота игрока 
 	bool bb=false;
 	int i,ci,cj,ki,kj,ii,colsize;
@@ -70,6 +73,72 @@ EXPORT bool  check_pos(float x,float y,float z,float h){ // x,y,z координ
 		}
 	}
 	
+
+	return bb;
+}
+
+
+....
+	
+	
+	
+EXPORT bool  check_pos_all(float x,float y,float z,float h){
+	bool bb=false;
+	int i,j,k;
+	Myline pl;
+	Mytriangle tr;
+	//unsigned int begs,ends;
+//	float ;
+	//begs = clock( );//UTIL_GetMilliseconds( );
+	//---------------------------------
+	//инициализация текущего состояния игрока
+	if(h==0){
+		player.Height=pheight;
+	}
+	else{
+		player.Height=h;
+	}
+player.Height=pheight;
+	player.point.x=x;
+	player.point.y=y;
+	player.point.z=z;
+
+		pl.points[0].x=player.point.x;
+		pl.points[0].z=player.point.z;
+		pl.points[0].y=player.point.y-(r2-0.1);
+		pl.points[1].x=player.point.x;
+		pl.points[1].z=player.point.z;
+		pl.points[1].y=player.point.y+(r2-0.1);
+
+		par.points[0].x=player.point.x-(r2);
+		par.points[0].z=player.point.z-(r2);
+//		par.points[0].y=player.point.y-(h2-r2);
+		par.points[0].y=player.point.y-(r2);
+
+		par.points[1].x=player.point.x-(r2);
+		par.points[1].z=player.point.z-(r2);
+//		par.points[1].y=player.point.y+(h2-r2);
+		par.points[1].y=player.point.y+(r2);
+
+		par.points[2].x=player.point.x+(r2);
+		par.points[2].z=player.point.z+(r2);
+//		par.points[2].y=player.point.y+(h2-r2);
+		par.points[2].y=player.point.y+(r2);
+
+		par.points[3].x=player.point.x+(r2);
+		par.points[3].z=player.point.z+(r2);
+//		par.points[3].y=player.point.y-(h2-r2);
+		par.points[3].y=player.point.y-(r2);
+	for(i=0;(!bb)&&(i<myobjs.objs.size());i++){
+		for(j=0;(!bb)&&(j<myobjs.objs[i].f.ff.size());j++){
+		/*	for(k=0;k<3;k++){
+				tr.points[k]=myobjs.objs[i].v.p[myobjs.objs[i].f.ff[j].fff[k]];
+			}*/
+			if(myobjs.objs[i].in_triangle(j,player,pl,par)){
+				bb=true;
+			}
+		}
+	}
 
 	return bb;
 }
